7b7015e87c4203bc71d289c69d1ed27c4d561a66
vandniBirla <78420545+vandniBirla@users.noreply.github.com>
2021-07-04 13:43:10 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function setup() {
 	
 	groundObject=new ground(width/2,670,width,20);
 	dustbinObj=new dustbin(1200,650);
-	paper=new Paper(400,666,20);
+	paper=new Paper(200,450,70);
 	Engine.run(engine);
 	
 }
@@ -35,6 +35,6 @@ function draw() {
 
 function keyPressed(){
 	if(keyCode===UP_ARROW){
-		Matter.Body.applyForce(paper.body,paper.body.position,{x:130,y:-145});
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:130,y:-300});
 	}
 }
